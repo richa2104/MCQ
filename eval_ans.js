594b414da99ee577f9db06d0acbb3f1ba01e5c57
Richa <richa.yadav@lge.com>
2021-03-21 16:18:10 +0530

    added

--- eval_ans.js
+++ eval_ans.js
@@ -27,11 +27,78 @@ function eval_ans() {
 	window.location.href = "Screen5.html";
     console.log(ans);
 }
-
+var a=1;
+var i=1;
+var b=10;
+var j=10;
 function next_que(){
-	// var h=document.getElementById("111");
-	// var i=document.getElementById("222");
-	// if (h.style.display === "none") {
-		document.getElementById(111).style.visibility = "hidden";
-		document.getElementById(222).style.visibility = "visible";
+	document.getElementById('button_prev').style.display = 'block';
+	for(i=1;1<=10;i++){
+		if(a==1){
+			document.getElementById(1).style.display = "none";
+			document.getElementById(a+1).style.display = "block";
+			a++;
+			console.log("next if a = " + a);
+			break;
+		}
+		else if(a>1 && a<=8){
+			a++;
+			document.getElementById(a).style.display = "block";
+			document.getElementById(a-1).style.display = "none";
+			console.log("next else if a = " + a);
+			break;
+		}
+		else{
+			a++;
+			document.getElementById(a).style.display = "block";
+			document.getElementById(a-1).style.display = "none";
+			document.getElementById('button_next').style.display = 'none';
+			document.getElementById('button_lang').style.display = 'block';
+			console.log("next else a = " + a);
+			break;
+		}
+	}
 }
+function prev_que(){
+	for(j=10;j>=1;j--){
+		if(a==10){
+			document.getElementById('button_next').style.display = 'block';
+			document.getElementById(a).style.display = "none";
+			document.getElementById(a-1).style.display = "block";
+			document.getElementById('button_lang').style.display = 'none';
+			a--;
+			console.log("prev if a = " + a);
+			break;
+		}
+		else if(a>2 || a==8){
+			document.getElementById('button_next').style.display = 'block';
+			document.getElementById(a).style.display = "none";
+			document.getElementById(a-1).style.display = "block";
+			document.getElementById('button_lang').style.display = 'none';
+			a--;
+			console.log("prev else if a = " + a);
+			break;
+		}
+		else{
+			document.getElementById('button_prev').style.display = 'none';
+			document.getElementById(a).style.display = "none";
+			document.getElementById(a-1).style.display = "block";
+			document.getElementById('button_lang').style.display = 'none';
+			a--;
+			console.log("prev else a = " + a);
+			break;
+		}
+	}
+}
+window.onload = function() {
+	for(let k=1; k<=10;k++){
+		if(k==1){
+			document.getElementById(k).style.display = 'block';
+			document.getElementById('button_prev').style.display = 'none';
+			document.getElementById('button_lang').style.display = 'none';
+		}
+		else{
+			document.getElementById(k).style.display = 'none';
+		}
+	}
+};
